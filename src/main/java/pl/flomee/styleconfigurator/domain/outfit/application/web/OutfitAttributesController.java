54a87a1af637b8376bf1f92f15c5d5d3c94725b3
PawelS <58605803+PawelSolecki@users.noreply.github.com>
2024-09-05 19:25:00 +0200

    optimize imports

--- src/main/java/pl/flomee/styleconfigurator/domain/outfit/application/web/OutfitAttributesController.java
+++ src/main/java/pl/flomee/styleconfigurator/domain/outfit/application/web/OutfitAttributesController.java
@@ -1,7 +1,6 @@
 package pl.flomee.styleconfigurator.domain.outfit.application.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
